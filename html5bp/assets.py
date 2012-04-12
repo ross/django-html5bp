da18399de39897b3c1bb511a90e351ff83640b8f
Ross McFarland <rwmcfa1@neces.com>
2012-04-12 10:48:00 -0700

    js/script.js should not be here

--- html5bp/assets.py
+++ html5bp/assets.py
@@ -5,4 +5,4 @@
 from django_assets import Bundle
 
 html5bp_css = Bundle('css/base.css')
-html5bp_js = Bundle('js/jquery-1.7.1.min.js', 'js/script.js')
+html5bp_js = Bundle('js/jquery-1.7.1.min.js')
