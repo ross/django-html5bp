5b20f5f3514d5850a81c9a9f29700e4a215043f9
Ross McFarland <rwmcfa1@neces.com>
2013-04-25 12:32:41 -0700

    app specific static directories

--- html5bp/assets.py
+++ html5bp/assets.py
@@ -4,5 +4,5 @@
 
 from django_assets import Bundle
 
-html5bp_css = Bundle('css/base.css')
-html5bp_js = Bundle('js/jquery-1.7.1.min.js')
+html5bp_css = Bundle('html5bp/css/base.css')
+html5bp_js = Bundle('html5bp/js/jquery-1.7.1.min.js')
